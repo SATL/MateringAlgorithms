fe089e14147e6df1b47f85536b67332c00fa4c5d
Eslem <j.eslem03@gmail.com>
2019-02-21 14:59:46 +0100

    resize array stack

---
+++ src/algorithms_I_princeton/bags_queues_stacks/ResizeArrStackString.java
@@ -0,0 +1,48 @@
+package algorithms_I_princeton.bags_queues_stacks;
+
+public class ResizeArrStackString {
+
+    String[] arr = new String[1];
+    int n = 0;
+
+
+    public void push(String item) {
+        if (needsToGrowth()) resize(capacityToIncrease());
+        arr[n++] = item;
+    }
+
+    private boolean needsToGrowth() {
+        return n == arr.length;
+    }
+
+    private int capacityToIncrease() {
+        return arr.length * 2;
+    }
+
+    public void resize(int capacity) {
+        String[] copy = new String[capacity];
+        for (int i = 0; i < arr.length; i++) {
+            copy[i] = arr[i];
+        }
+        arr = copy;
+    }
+
+    public String pop() {
+        String item = arr[--n];
+        arr[n] = null;
+
+        if(needsToGrowth())
+            resize(capacityToShrink());
+        return item;
+    }
+
+    private boolean needsShrink(){
+        return n>0 && n == arr.length/4;
+    }
+
+    private int capacityToShrink(){
+        return arr.length/2;
+    }
+
+
+}
